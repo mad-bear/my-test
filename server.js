6427c626856f891b33d2f5cf53e3d0a1f582cb86
mh <mre.ezboy3@gmail.com>
2017-06-20 01:56:48 +0330

    portal portfolio v1

--- server.js
+++ server.js
@@ -1,56 +1,23 @@
 const express = require('express');
 const hbs = require('hbs');
-const fs = require('fs');
 
 const port = process.env.PORT || 3000;
 var app = express();
 
-hbs.registerPartials(__dirname + '/views/partials')
-app.set('view engine', 'hbs');
-
-app.use((req, res, next) => {
-  var now = new Date().toString();
-  var log = `${now}: ${req.method} ${req.url}`;
-
-  console.log(log);
-  fs.appendFile('server.log', log + '\n');
-  next();
-});
-
-// app.use((req, res, next) => {
-//   res.render('maintenance.hbs');
-// });
-
-app.use(express.static(__dirname + '/public'));
-
-hbs.registerHelper('getCurrentYear', () => {
+hbs.registerHelper('curYear',() => {
   return new Date().getFullYear();
 });
-
-hbs.registerHelper('screamIt', (text) => {
-  return text.toUpperCase();
+hbs.registerHelper('curDate',() => {
+  return new Date().toString();
 });
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
-  res.render('home.hbs', {
-    pageTitle: 'Home Page',
-    welcomeMessage: 'Welcome to my website'
-  });
+  res.render('home.hbs');
 });
 
-app.get('/about', (req, res) => {
-  res.render('about.hbs', {
-    pageTitle: 'About Page'
-  });
-});
-
-// /bad - send back json with errorMessage
-app.get('/bad', (req, res) => {
-  res.send({
-    errorMessage: 'Unable to handle request'
-  });
-});
-
+var date = new Date();
 app.listen(port, () => {
-  console.log(`Server is up on port ${port}`);
+  console.log(port + `:port - runned at ${date}`);
 });
